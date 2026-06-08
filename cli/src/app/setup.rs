4b59f9e64e1cb1cfbeca020f82c84f8fc516f915
drosseau-ivision <drosseau@ivision.com>
2026-06-08 15:12:13 -0600

    cli: Update gradle to use 9.3.1, fully qualified instead of 9.3

--- cli/src/app/setup.rs
+++ cli/src/app/setup.rs
@@ -21,7 +21,7 @@ pub struct Setup {
     force: bool,
 
     /// Set the gradle version
-    #[arg(long, default_value_t = String::from("9.3"))]
+    #[arg(long, default_value_t = String::from("9.3.1"))]
     gradle_version: String,
 
     /// Set the project name, otherwise generated from the device
